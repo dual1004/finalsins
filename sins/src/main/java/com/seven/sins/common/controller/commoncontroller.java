879f75e79d187211b925b59f04e503de0f00540a
jeon byung geol <dual1002@naver.com>
2017-02-21 18:47:26 +0900

    sens3

--- sins/src/main/java/com/seven/sins/common/controller/commoncontroller.java
+++ sins/src/main/java/com/seven/sins/common/controller/commoncontroller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class commoncontroller {
 
+	//단순 a링크
 	@RequestMapping("alink.do")
 	public String alink(String path){
 		return path;
